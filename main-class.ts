47cddb96c502f4974ab07fabc116e1534ff30105
geno0707 <geno0707@rocketmail.com>
2026-06-22 21:18:20 -0400

    Update main-class.ts

--- main-class.ts
+++ main-class.ts
@@ -12,15 +12,22 @@ type Screen = 'game' | 'menu' | 'hiredList' | 'firedList' | 'settings';
 
 
 class mainClass{
-private pointer = 0;
+private pointer: Record<Screen, number> = {
+            'game': 0,
+            'hiredList': 0,
+            'firedList': 0,
+            'menu': 0,
+            'settings': 0
+        };
 private people: TrimmedUser[] = [];
 private hired: TrimmedUser[] = [];
 private fired: TrimmedUser[] = [];
-private isInMenu: boolean = false;
 private started: boolean = false;
 private screen: Screen = 'game';
 
+
     constructor(){
+
     }
 
     async load(){
@@ -47,24 +54,30 @@ private screen: Screen = 'game';
            switch (this.screen){
             case 'game':{
                 this.mainGameState(str, key);
+                break;
             }
             case 'menu':{
                 this.menu(str, key);
+                break;
             }
             case 'firedList':{
                 this.firedListState(str, key);
+                break;
             }
             case 'hiredList':{
                 this.hiredListState(str, key);
+                break;
             }
             case 'settings':{
                 this.settingState(str, key);
+                break;
             }
            }
            
     }); 
 }
     private mainGameState(str: string, key: any){
+        const person = this.current();
         if (key.name === 'a') {
             console.log(this.prev());
         }
@@ -74,13 +87,15 @@ private screen: Screen = 'game';
         if (key.name === 'return') {
             //hire
             //splice the current and push to hired.
-            this.hired.push(this.current());
-            this.people.splice(this.pointer, 1);
+            if (!person) return;
+            this.hired.push(person);
+            this.people.splice(this.pointer[this.screen], 1);
         }
         if (key.name === 'backspace') {
             //fire
-            this.fired.push(this.current());
-            this.people.splice(this.pointer, 1);
+            if (!person) return;
+            this.fired.push(person);
+            this.people.splice(this.pointer[this.screen], 1);
         }
         if (key.name === 'q'){ // open menu
             this.screen = 'menu';
@@ -92,20 +107,35 @@ private screen: Screen = 'game';
             let newPointer = 0;
             console.log("Press 1 for hired list, and 2 for fired list, q to exit menu");
             if (key.name === '1'){
-                console.log(`you have hired total of: ${this.hired.length} people`);
+                console.log(`you have hired total of: ${this.hired.length} people.`);
+                this.screen = 'hiredList';
             }
             if (key.name === '2'){
-                console.log(`you have hired total of: ${this.fired.length} people`);
+                console.log(`you have fired total of: ${this.fired.length} people`);
+                this.screen = 'firedList';
             }
             if (key.name === 'q'){
-                this.isInMenu = false;         
+                this.screen = 'game';
+                console.log("exiting menu");
             }
         };
 
-    private firedListState(str: string, key: any){
 
-    }
     private hiredListState(str: string, key: any){
+        let newPointer = 0;
+        if (key.name === 'a') {
+            console.log(this.prev());
+        }
+        if (key.name === 'd') {
+            console.log(this.next());
+        }
+        if (key.name === 'q'){ // open menu
+            this.screen = 'menu';
+            console.log("now entering menu.");
+        }
+    }
+
+    private firedListState(str: string, key: any){
 
     }
     private settingState(str: string, key: any){
@@ -114,21 +144,25 @@ private screen: Screen = 'game';
 
     // mechanics.    
     private current(){
-        return this.people[this.pointer];
+        switch (this.screen){
+            case ('game'): return this.people[this.pointer[this.screen]];
+            case ('hiredList'): return this.hired[this.pointer[this.screen]];
+            case ('firedList'): return this.fired[this.pointer[this.screen]];
+        }
     }
 
     private next(){
-        this.pointer++
-        if (this.pointer >= this.people.length) this.pointer = 0;
+        this.pointer[this.screen]++;
+        if (this.pointer[this.screen] >= this.people.length) this.pointer[this.screen] = 0;
         return this.current();
     }
 
     private prev(){
-        this.pointer--
-        if (this.pointer < 0) {
-            this.pointer = this.people.length - 1;
+        this.pointer[this.screen]--;
+        if (this.pointer[this.screen] < 0) {
+            this.pointer[this.screen] = this.people.length - 1;
         }
-        return this.people[this.pointer]
+        return this.people[this.pointer[this.screen]];
     }
 
     private async loadNew(){ // load new profiles.from git-analysis.ts
